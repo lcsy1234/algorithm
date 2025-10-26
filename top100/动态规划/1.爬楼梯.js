8775523f30cb901224277765b61081f7f6a43b2a
lcsy1234 <2485531443@qq.com>
2025-10-26 23:13:47 +0800

    动态规划 简单

--- top100/动态规划/1.爬楼梯.js
+++ top100/动态规划/1.爬楼梯.js
@@ -2,12 +2,18 @@
 function climbStairs(n) {
     if(n<=2) return n
     //定义状态，dp里面是累积次数
-   const dp=new Array(n+1)
+   const dp=new Array(n+1).fill(0)
    dp[1]=1
    dp[2]=2
    for(let i=3;i<=n;i++){
     dp[i]=dp[i-1]+dp[i-2]
    }
+   console.log("%c Line:12 🍐 dp[n]", "color:#b03734", dp[n]);
    return dp[n]
 }
+climbStairs(4)
 //递归
+// 思路：
+
+
+
